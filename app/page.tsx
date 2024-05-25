39b3e804aba428e201f7af255492219ed14a59b8
ariskaAdi <ariskaadiprasetyo@gmail.com>
2024-05-25 21:04:14 +0700

    complete next gemini ai

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,23 @@
 "use client";
 
 import { useChat } from "ai/react";
-import { Send } from "lucide-react";
+import { Bot, Loader, Loader2, MoreHorizontal, Plus, Send, User2, X } from "lucide-react";
+import Image from "next/image";
+import Markdown from "./component/markdown";
+import { ChangeEvent, useState } from "react";
+import SelectedImages from "./component/selectedImages";
+import Messages from "./component/messages";
+import InputForm from "./component/inputForm";
 
 export default function Home() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } = useChat({
     api: "api/genai",
   });
+
   return (
-    <main className="flex min-h-screen flex-col items-center  p-12">
-      {/* form element */}
-      {RenderForm()}
-      {RenderMessages()}
-      {/* rendering messges */}
+    <main className="flex min-h-screen flex-col items-center p-12 text-lg">
+      <InputForm input={input} handleInputChange={handleInputChange} handleSubmit={handleSubmit} isLoading={isLoading} stop={stop} />
+      <Messages messages={messages} isLoading={isLoading} />
     </main>
   );
 }
-
-function RenderForm() {
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        handleSubmit(event, {
-          data: {
-            propmt: input,
-          },
-        });
-      }}
-      className="w-full flex flex-row gap-2 items-center h-full"
-    >
-      <input
-        type="text"
-        placeholder="ask something..."
-        value={input}
-        onChange={handleInputChange}
-        className="border-b border-dashed outline-none w-full px-4 py-2 text-[#0842a0] placeholder:text-[#0842a099] text-right focus:placeholder-transparent"
-      />
-      <button type="submit" className="rounded-full shadow-md border flex flex-row">
-        <Send className="p-3 h-10 w-10 stroke-stone-500" />
-      </button>
-      {/* input */}
-      {/* send button */}
-    </form>
-  );
-}
-
-function RenderMessages() {}
